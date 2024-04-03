7ca344c194129003d1a577c8a6ff61cc07c72e11
foncheto <alfopinto@alumnos.uai.cl>
2024-04-03 15:54:35 -0300

    Updated structure for new apis

---
+++ backend/storage_api/src/app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const path = require("path");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5050;
+const mongo_login_string = process.env.mongo_login_string;
+
+// Connect to MongoDB
+mongoose
+  .connect(mongo_login_string, {})
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
+
+// Enable CORS
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+// Serve static files from the React app
+app.use(
+  express.static(
+    path.join(__dirname, "../../../educaiCRM-Front/frontend/build")
+  )
+);
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Serve the React app
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../../../educaiCRM-Front/frontend/build/index.html")
+  );
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
