b4769cf17b1045b679bca197e33a2a9de1d563c4
foncheto <alfopinto@alumnos.uai.cl>
2024-05-17 12:24:18 -0400

    cleaned up failed express openai api

--- backend/openai_api/src/routes/openaiRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const openaiController = require("../controllers/openaiController");
-
-// Define routes
-router.post("/generate-text", openaiController.generateText);
-
-module.exports = router;
