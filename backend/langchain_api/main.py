b5a05250a06f424a85d3279e19f7f0676efa519a
foncheto <alfopinto@alumnos.uai.cl>
2024-05-17 12:23:41 -0400

    Added fastapi boilerplate for RAG Service Langchain

--- backend/langchain_api/main.py
+++ backend/langchain_api/main.py
@@ -2,9 +2,12 @@ from fastapi import FastAPI, HTTPException # type: ignore
 import os
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
 
-llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
 load_dotenv()   # Load environment variables from .env file
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
+chat = ChatOpenAI(api_key=openai_api_key, model="gpt-3.5-turbo-0125")
 
 app = FastAPI()
 
@@ -14,5 +17,11 @@ async def root():
 
 @app.get("/testing")
 async def testing():
-    
-    return {"message": "Testing"}
+    response = chat.invoke(
+        [
+            HumanMessage(
+                content="Translate this sentence from English to French: I love programming."
+            )
+        ]
+    )
+    return {"reply": response}
